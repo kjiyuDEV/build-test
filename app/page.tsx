1e0a89a2ace22908e5aed9062b7f13f94c4b7c8a
kjiyuDEV <kjiyu1379@gmail.com>
2025-03-11 16:27:54 +0900

    last commit in this repo

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function MBTITest() {
         <div className="max-w-4xl mx-auto space-y-8">
           <div className="text-center mb-12">
             <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-4">
-              우영미 메롱😛
+              응답지 조정중😛
             </h1>
             <p className="text-gray-600">김지유가 젤 쎔</p>
           </div>
